97f6f7af70ea7c5ab5c7e89da6fd5b37e1734904
digitalsmart911-eng <digitalsmart911@gmail.com>
2026-09-15 16:35:54 +0700

    Update Supabase publishable key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://ycubidshrzofbhcrryhx.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "GANTI_DENGAN_PUBLISHABLE_KEY_ANDA";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_ztZQ2kh6xbH_JgKvgUGC5g_WYxEWocB";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
